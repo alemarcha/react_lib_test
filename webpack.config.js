5998384aaaa6c8332d362d87c84e5d33d9d64791
Alexis Martínez Chacón <alexismartinezchacon@gmail.com>
2018-05-28 11:47:27 +0200

    update package name for npm

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
     output: {
         path: __dirname + "/dist",
         filename: 'bundle.js',
-        library: 'hellolib',
+        library: 'hellolibalemarcha',
         libraryTarget: 'umd'
     },
     module: {
